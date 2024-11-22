93cc3d155940f82ada568b189cc0129891f51be4
Aldric Adams <aldricadams12@gmail.com>
2024-11-22 11:36:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { RectComponent } from "./rect/rect/rect.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, RectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'two-way-binding';
+  rectSize = {
+    width: '200',
+    height: '100'
+  };
 }
